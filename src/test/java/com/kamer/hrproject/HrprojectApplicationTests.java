8f36475564fbdc475a86f54cae30055a7a7d8e79
Kamer <k.elciyar@gmail.com>
2019-06-24 15:19:42 +0300

    Initial commit

---
+++ src/test/java/com/kamer/hrproject/HrprojectApplicationTests.java
@@ -0,0 +1,16 @@
+package com.kamer.hrproject;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class HrprojectApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
