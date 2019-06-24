2afc78b95336f17105dc147196dcfb2df511862d
Kamer <k.elciyar@gmail.com>
2019-06-24 17:44:18 +0300

    Fix entity issues.

--- src/main/java/com/kamer/hrproject/entities/Application.java
+++ src/main/java/com/kamer/hrproject/entities/Application.java
@@ -3,11 +3,13 @@ package com.kamer.hrproject.entities;
 import lombok.*;
 
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.ManyToOne;
 
 @Entity
 @Getter
 @Setter
-@ToString
+@ToString(callSuper = true)
 @AllArgsConstructor
 @NoArgsConstructor
 public class Application extends BaseEntity {
@@ -18,5 +20,8 @@ public class Application extends BaseEntity {
     private String address;
     private String thoughts;
     private byte[] resume;
+    @ManyToOne(fetch = FetchType.EAGER)
+    @NonNull
+    private Job job;
 
 }
--- src/main/java/com/kamer/hrproject/entities/Job.java
+++ src/main/java/com/kamer/hrproject/entities/Job.java
@@ -2,19 +2,25 @@ package com.kamer.hrproject.entities;
 
 import lombok.*;
 
+import javax.persistence.CascadeType;
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.OneToMany;
 import java.util.Date;
+import java.util.Set;
 
 @Entity
 @Getter
 @Setter
-@ToString
-@AllArgsConstructor
+@ToString(exclude = "applications", callSuper = true)
 @NoArgsConstructor
 public class Job extends BaseEntity {
     private String jobTitle;
     private String jobDescription;
     private Integer quota;
     private Date lastApplicationDate;
+    @OneToMany(fetch = FetchType.LAZY, cascade = CascadeType.REMOVE)
+    @Setter(AccessLevel.NONE)
+    private Set<Application> applications;
 
 }
